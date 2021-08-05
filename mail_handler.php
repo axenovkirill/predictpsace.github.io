864a24470055c07cf1c53b9ba28fc016581a68b1
axenovkirill <42481607+axenovkirill@users.noreply.github.com>
2021-08-05 21:48:59 +0300

    Add files via upload

---
+++ mail_handler.php
@@ -0,0 +1,18 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "kirill.axenov@predictspace.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['name'];
+    $subject = "PredictSpace";
+    $price = $_POST['price'];
+    $location = $_POST['location'];
+    $status = $_POST['status']
+    $message = $first_name . " " . $location . " " .$status  . " " . $price" wrote you" . "\n\n";
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
+?>
